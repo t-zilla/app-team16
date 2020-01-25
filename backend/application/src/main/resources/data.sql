73340b66da06374d1ab49e61e5765ad7006407aa
Grzegorz Stala <gstala01@gmail.com>
2020-01-25 17:24:34 +0100

    Feat: initial data.sql

---
+++ backend/application/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO degree_course (name, study_language) VALUES
+('Informatyka Stosowana', 'POLISH'),
+('Zarządzanie', 'POLISH'),
+('Inżynieria systemów', 'POLISH'),
+('Computer science', 'ENGLISH');
+
+INSERT INTO speciality (name, degree_course_id) VALUES
+('Inżynieria oprogramowania', 1),
+('Danologia', 1);
--- backend/universitydomain/src/main/java/com/psi/degreecourse/model/DegreeCourse.java
+++ backend/universitydomain/src/main/java/com/psi/degreecourse/model/DegreeCourse.java
@@ -4,6 +4,8 @@ import lombok.Data;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -20,6 +22,7 @@ public class DegreeCourse {
     private Long id;
     @Column(name = "name")
     private String name;
+    @Enumerated(EnumType.STRING)
     @Column(name = "study_language")
     private StudyLanguage studyLanguage;
 
