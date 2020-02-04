b091b377f6a4e8a70f84f468aefe0d8981ca8d17
Grzegorz Stala <gstala01@gmail.com>
2020-02-04 22:43:51 +0100

    Fix: Javers for Syllabus

--- backend/universitydomain/src/main/java/com/psi/syllabus/repository/SyllabusRepository.java
+++ backend/universitydomain/src/main/java/com/psi/syllabus/repository/SyllabusRepository.java
@@ -1,10 +1,11 @@
 package com.psi.syllabus.repository;
 
 import com.psi.syllabus.model.Syllabus;
+import org.javers.spring.annotation.JaversSpringDataAuditable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@JaversSpringDataAuditable
 public interface SyllabusRepository extends JpaRepository<Syllabus, Long>, JpaSpecificationExecutor<Syllabus> {
 }
